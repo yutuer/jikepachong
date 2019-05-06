2d3352a15498f80b4b827c2b9b2848771f72171b
yutuer <yutuerzf@163.com>
2019-05-07 02:28:38 +0800

    home

--- src/util/workGroup.go
+++ src/util/workGroup.go
@@ -23,6 +23,31 @@ func exam1() {
 
 func exam2() {
 	total := 100
+	diff := -10
+
+	wg := sync.WaitGroup{}
+	//wg 添加一个负数
+	//1. 先添加100, 在加-10是正确的, Add参数可以为负数, 运行90次
+	wg.Add(total)
+	wg.Add(diff)
+
+	//2. 先添加-10, 在加100是错误的
+	//wg.Add(diff)
+	//wg.Add(total)
+
+	for i := 0; i < total+diff; i++ {
+		go func(i int) {
+			fmt.Println(i)
+			wg.Done()
+		}(i)
+	}
+
+	wg.Wait()
+}
+
+// 可以重复利用
+func exam3() {
+	total := 100
 
 	wg := sync.WaitGroup{}
 	wg.Add(total)
@@ -35,4 +60,48 @@ func exam2() {
 	}
 
 	wg.Wait()
+
+	fmt.Println("once again!!")
+
+	wg.Add(total)
+	// 周期走完, 再次利用
+	for i := 0; i < total; i++ {
+		go func(i int) {
+			fmt.Println(i)
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+
+}
+
+// 利用future来获取结果
+func exam4() {
+	const total = 10
+
+	var results [total]int
+
+	wg := sync.WaitGroup{}
+	wg.Add(total)
+
+	for i := 0; i < total; i++ {
+		go func(i int) {
+			fmt.Println(i)
+			wg.Done()
+
+			results[i] = i
+		}(i)
+	}
+
+	wg.Wait()
+
+	fmt.Println(results)
+}
+
+func exam5() {
+	once := sync.Once{}
+
+	go once.Do(func() { fmt.Println(1) })
+	go once.Do(func() { fmt.Println(2) })
+
 }
--- src/util/workGroup_test.go
+++ src/util/workGroup_test.go
@@ -5,3 +5,19 @@ import "testing"
 func TestExam1(t *testing.T) {
 	exam1()
 }
+
+func TestExam2(t *testing.T) {
+	exam2()
+}
+
+func TestExam3(t *testing.T) {
+	exam3()
+}
+
+func TestExam4(t *testing.T) {
+	exam4()
+}
+
+func TestExam5(t *testing.T) {
+	exam5()
+}
